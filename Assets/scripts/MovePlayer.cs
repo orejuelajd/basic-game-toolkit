5627f42af514deb6de125e1b3445621eb46c892a
Juan David Orejuela Bolanos <juan.orejuela@hotmail.com>
2016-07-12 23:23:41 -0500

    First commit

---
+++ Assets/scripts/MovePlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+	public float multiplier = 0.0f;
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Input.GetAxis("Horizontal") * multiplier,0f,Input.GetAxis("Vertical") * multiplier);
+	}
+}
